443b50f9d4d633650d5fed7b0d773a501d95beff
José <jose.c.junior88@aluno.senai.br>
2026-05-26 10:28:19 -0300

    Update verificacaologin.js

--- site/public/assets/js/verificacaologin.js
+++ site/public/assets/js/verificacaologin.js
@@ -22,7 +22,7 @@ function logout() {
     localStorage.removeItem('usuarioAtual');
     
     // Força a URL a resetar a partir da raiz do GitHub, limpando caminhos antigos
-    window.location.href = "../../pages/index.html";
+    window.location.href = "../../../public/pages/index.html";
 }
 
 function redirecionarUsuario() {
@@ -33,7 +33,7 @@ function redirecionarUsuario() {
         window.location.href = "../../../app/app.html";
     } else {
         // CORREÇÃO: Força o caminho limpo direto para o login
-        window.location.href = "../../pages/index.html";
+        window.location.href = "../../../public/pages/index.html";
     }
 }
 
