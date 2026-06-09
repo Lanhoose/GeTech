0a09e1437ae6079b935673dbe8ec1a99042faca4
José <jose.c.junior88@aluno.senai.br>
2026-06-09 16:38:12 -0300

    Update login.js

--- site/public/assets/js/login.js
+++ site/public/assets/js/login.js
@@ -69,7 +69,7 @@ function fazerLogin() {
         localStorage.setItem('usuarioAtual', usuario.email);
 
         setTimeout(() => {
-            window.location.href = `../../public/pages/redimensionamento_app.html`;
+            window.location.href = `${window.BASE_URL}/site/public/pages/redimensionamento_app.html`;
         }, 1000);
     } else {
         mostrarMensagem(msg, 'Email ou senha incorretos!', 'erro');
