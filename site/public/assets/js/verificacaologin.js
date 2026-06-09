2769eab14800d57dfc0e211f74cc053dd0c9527d
José <jose.c.junior88@aluno.senai.br>
2026-06-09 16:08:05 -0300

    Update verificacaologin.js

--- site/public/assets/js/verificacaologin.js
+++ site/public/assets/js/verificacaologin.js
@@ -59,7 +59,7 @@ function logout() {
     localStorage.removeItem('usuarioAtual');
 
     // Redireciona usando a base global limpa
-    window.location.href = `../../../Site C/public/pages/redirecionar.html`;
+    window.location.href = `${window.BASE_URL}/site/public/pages/index.html`;
 }
 
 function redirecionarUsuario() {
